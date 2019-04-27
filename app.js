26c26fae70494fd2c7bd69e042f8bc224dced619
Nuo <nuot@uci.edu>
2019-04-26 22:24:58 -0700

    add more pics and functions

---
+++ app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAToBlUA1_v1Zq0uULd5vwqKU9Mx7VJ7O4",
+    authDomain: "jewelry-gallery-project.firebaseapp.com",
+    databaseURL: "https://jewelry-gallery-project.firebaseio.com",
+    projectId: "jewelry-gallery-project",
+    storageBucket: "jewelry-gallery-project.appspot.com",
+    messagingSenderId: "499903494184"
+  };
+  firebase.initializeApp(config);
